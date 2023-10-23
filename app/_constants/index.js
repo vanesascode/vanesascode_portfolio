aa8e910837dc57b3941d0029d87c32f601c998a9
vanesascode <vanesa.juarez.paris@gmail.com>
2023-10-23 21:54:16 +0200

    added details to the projects preview page

--- app/_constants/index.js
+++ app/_constants/index.js
@@ -1,40 +1,64 @@
 
 export const projectsPreviews = [
   {
-    id: "pp-1",
+    id: "pp-6",
     title:
       "Dev School",
     desc: "Educational & blog website",
     img: "projects/cover-dev-school.jpg",
     video: "/projects/video-dev-school.gif",
     frame: "/projects/thinkpadframepreview.png",
+    link: "https://devschool-vanesascode.vercel.app/"
   },
   {
-    id: "pp-2",
+    id: "pp-5",
     title:
       "Lizz Boots",
     desc: "Showcase landing page",
     img: "projects/cover-lizz-boots.jpg",
     video: "/projects/video-lizz-boots.gif",
     frame: "/projects/thinkpadframepreview.png",
+    link: "https://lizz-boots-vanesascode.vercel.app/"
   },
   {
-    id: "pp-3",
+    id: "pp-4",
     title:
       "Zen Todo List",
     desc: "Todo list app",
     img: "projects/cover-todo-list.jpg",
     video: "/projects/video-todo-list.gif",
     frame: "/projects/thinkpadframepreview.png",
+    link: "https://zen-todo-list-vite-typescript-classes.vercel.app/"
   },
   {
-    id: "pp-4",
+    id: "pp-3",
+    title:
+      "Everyday Zen",
+    desc: "Showcase landing page",
+    img: "projects/cover-everyday-zen.jpg",
+    video: "/projects/video-everyday-zen.gif",
+    frame: "/projects/thinkpadframepreview.png",
+    link: "https://vanesascode.github.io/web-selling-book-with-bootstrap-sass/"
+  },
+  {
+    id: "pp-2",
     title:
       "Contact List",
-    desc: "App using 4geeks restful api",
+    desc: "4geeks exercise app using restful api",
     img: "projects/cover-contact-list.jpg",
     video: "/projects/video-contact-list.gif",
     frame: "/projects/thinkpadframepreview.png",
+    link: "https://contact-list-react-router-context-api-tsx-vanesascode.vercel.app"
+  },
+  {
+    id: "pp-1",
+    title:
+      "Seconds Counter",
+    desc: "4geeks exercise app",
+    img: "projects/cover-counter.jpg",
+    video: "/projects/video-counter.gif",
+    frame: "/projects/thinkpadframepreview.png",
+    link: "https://life-is-just-seconds-vanesascode.vercel.app/"
   },
 
 ];
